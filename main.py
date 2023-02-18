3fd61a40cf644950e20229429dd123e137fed22f
Seth Christmus <seth.christmus@fdmgroup.com>
2023-02-18 13:16:11 -0500

    added greeting and source for ascii art

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
 
+print("Welcome to Rock, Paper, Scissors!")
+print("Developed by Seth Christmus.")
+print("ASCII art source: https://gist.github.com/wynand1004/b5c521ea8392e9c6bfe101b025c39abe")
+
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
 computer_choice = random.randint(0, 2)
 
